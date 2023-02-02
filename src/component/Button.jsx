ec910be76c19d11281be917cb21c46f4641bb8db
Clara <smile629639@gmail.com>
2023-02-02 16:50:50 +0800

    edit/button color

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -1,11 +1,12 @@
 function Button() {
   const ButtonStyle = {
-    background: "#1f57a6",
+    background: "#7851a9",
     color: "#ffffff",
     borderRadius: "4px",
     padding: "8px",
     width: "100%",
     border: "1px solid transparent",
+    cursor: "pointer",
   };
 
   return (
